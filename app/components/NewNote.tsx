d7b57ce05051882c2ce2411be4997fe01a320089
Kasib <kasibabdullah8@gmail.com>
2024-01-12 19:47:34 -0500

    caught up to work copy in typescript

---
+++ app/components/NewNote.tsx
@@ -0,0 +1,32 @@
+import { ReactElement } from "react";
+import newNoteStyles from "./NewNote.css";
+
+interface LinkProps {
+  rel: string;
+  href: string;
+}
+
+function NewNote(): ReactElement {
+  return (
+    <form id="note-form" method="post">
+      <p>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" required />
+      </p>
+      <p>
+        <label htmlFor="content">Content</label>
+        <input type="text" id="content" name="content" required />
+      </p>
+      <div className="form-actions">
+        <button>Add Note</button>
+      </div>
+    </form>
+  );
+}
+
+export default NewNote;
+
+// return type is an arraw of objects conforming to LinkProps
+export function links(): LinkProps[] {
+  return [{ rel: "stylesheet", href: newNoteStyles }];
+}
